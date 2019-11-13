ef92662330e7352f0eaa9b6b2a6855f40706830c
Hugo DENIAU <deniauh@pc5301-test.esiee.fr>
2019-11-13 10:44:52 +0100

    Changed SQL

--- resources/procedures/mises.sql
+++ resources/procedures/mises.sql
@@ -1,4 +1,3 @@
-ajout_mise_ticket(xident varchar, xpwd varchar, xid_ticket integer,xid_course integer, xnumero integer, xposition integer)
 
 CREATE OR REPLACE PROCEDURE ajout_mise_ticket(
   xident IN varchar,
@@ -8,14 +7,15 @@ CREATE OR REPLACE PROCEDURE ajout_mise_ticket(
   xnumero IN integer,
   xposition IN integer
 ) AS
-id INTEGER
-wrong_user_or_password EXCEPTION
+id INTEGER;
+wrong_user_or_password EXCEPTION;
+
 BEGIN
   id := get_id(XIDENT, XPWD);
 
-  IF id != 0 THEN
-    INSERT INTO dossards VALUES(xid_course, xnumero, xposition, )
+  IF (id != 0) THEN
+    INSERT INTO dossards VALUES(xid_course, xnumero, xposition, (SELECT nom FROM personnes WHERE id_personne=id), id);
   ELSE
     RAISE wrong_user_or_password;
-
+  END IF;
 END;
